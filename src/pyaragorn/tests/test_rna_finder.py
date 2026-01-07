9227e5d205214cd01bfb1c2cbd30ba6f9b9e6734
Martin Larralde <martin.larralde@embl.de>
2026-01-08 00:00:28 +0100

    Add tests to compare `aragorn -ps95` output

--- src/pyaragorn/tests/test_rna_finder.py
+++ src/pyaragorn/tests/test_rna_finder.py
@@ -67,3 +67,38 @@ class TestRNAFinder(unittest.TestCase):
         finder = RNAFinder(translation_table=11, tmrna=True, trna=False)
         for gene in finder.find_rna(str(record.seq)):
             self.assertIsInstance(gene, TMRNAGene)
+
+    def test_ps95(self):
+        record = data.load_record("CP001621.fna.gz")
+        lines = data.load_text("CP001621.ps95.txt").splitlines()
+        
+        finder = RNAFinder(translation_table=11, threshold_scale=0.95)
+        genes = finder.find_rna(str(record.seq))
+
+        for gene, expected in itertools.zip_longest(genes, batched(lines[2:], 3)):
+            self.assertIsNotNone(gene)
+            self.assertIsNotNone(expected)
+            result, seq, ss = expected
+            if gene.type == "tRNA":
+                matched = _TRNA_RX.match(result)
+                _, aa, complement, begin, end, energy, offset, anticodon = matched.groups()
+                self.assertEqual(gene.amino_acid, aa)
+                self.assertEqual(gene.begin, int(begin))
+                self.assertEqual(gene.end, int(end))
+                self.assertEqual(gene.anticodon_offset, int(offset))
+                self.assertEqual(gene.anticodon_length, len(anticodon))
+                self.assertEqual(gene.anticodon, anticodon)
+                self.assertEqual(gene.strand, -1 if complement == "c" else +1)
+                self.assertAlmostEqual(gene.energy, float(energy), places=1)
+                self.assertEqual(gene.sequence().lower(), seq)
+            elif gene.type == "tmRNA":
+                matched = _TMRNA_RX.match(result)
+                _, complement, begin, end, energy, orf_start, orf_end, peptide = matched.groups()
+                self.assertEqual(gene.begin, int(begin))
+                self.assertEqual(gene.end, int(end))
+                self.assertEqual(gene.orf_offset, int(orf_start))
+                self.assertEqual(gene.orf_offset + gene.orf_length, int(orf_end))
+                self.assertEqual(gene.peptide(), peptide)
+                self.assertEqual(gene.strand, -1 if complement == "c" else +1)
+                self.assertAlmostEqual(gene.energy, float(energy), places=1)
+                # self.assertEqual(gene.sequence().lower(), seq) # TODO
